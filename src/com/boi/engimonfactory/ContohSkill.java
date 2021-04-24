c4dcd0a5f26c42a1950b25e3a969de8951022efe
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2021-04-24 14:32:06 +0700

    update Inventory dan Player
    update Inventory dan Player

---
+++ src/com/boi/engimonfactory/ContohSkill.java
@@ -0,0 +1,50 @@
+package com.boi.engimonfactory;
+
+public class ContohSkill implements Storeable {
+    /* Placeholder atribut */
+    private String name; // nama skill
+    private String element; // elemen skill
+    private Integer bp; // Base Power skill
+    public ContohSkill(String name, String element, Integer bp) {
+        this.name = name;
+        this.element = element;
+        this.bp = bp;
+    }
+    
+    /* Tolong yang di bawah ini ada semua */
+    /* Implementasi interface Storeable */
+    /* Ini di copy-paste, disesuaikan */
+    /* Tolong override ini semua */
+
+    @Override
+    public String toString() {
+        return ("Skill " + this.name + "Element: "
+            + this.element + " BP: " + this.bp);
+    }
+
+    @Override
+    public void print() {
+        /* Kurang tau format printnya gimana jadi mungkin beginin aja udah cukup */
+        System.out.println("Skill " + this.name + " Element: " 
+            + this.element + " BP: " + this.bp);
+    }
+
+    /* Tolong override ini berdasarkan nama skill */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof ContohSkill)) return false;
+        ContohSkill other = (ContohSkill) o;
+        return this.name.equals(other.name);
+    }
+
+    @Override
+    public Integer getSortInt() {
+        return this.bp * -1; // Ini kopas aja sama persis
+    }
+
+    @Override
+    public String getSortStr() {
+        return ""; // Ini kopas aja sama persis
+    }
+}
---
+++ src/com/boi/engimonfactory/Storeable.java
@@ -0,0 +1,7 @@
+package com.boi.engimonfactory;
+
+public interface Storeable {
+    public void print();
+    public Integer getSortInt();
+    public String getSortStr();
+}
