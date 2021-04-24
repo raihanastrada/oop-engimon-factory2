743204973985c920828efa01f394c96d0d7e0794
Govindabhakta <igovindabhakta@gmail.com>
2021-04-24 21:19:51 +0700

    Update EngimonFactory2.java

--- src/com/boi/engimonfactory/EngimonFactory2.java
+++ src/com/boi/engimonfactory/EngimonFactory2.java
@@ -3,5 +3,6 @@ package com.boi.engimonfactory;
 public class EngimonFactory2 {
     public static void main(String[] args) {
         System.out.println("FUCK YOU AND YOUR FRIENDS");
+        System.out.println("FUCK YOU AND THE GUI");
     }
 }
